85061a3a9cc59b9dfed17b12b5b4aee1e536977e
Ian McPhail <webguyian@gmail.com>
2022-10-06 10:37:14 -0400

    Create CollapsibleSection component

---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header>
+      <span v-if="open" @click="open = !open">&#x25B2; Collapse</span>
+      <span v-if="!open" @click="open = !open">&#x25BC; Expand</span>
+    </header>
+    <slot v-if="open"><p>No content</p></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return {
+      open: true
+    };
+  }
+};
+</script>
+
+<style scoped>
+header {
+  background-color: gray;
+  padding: 3px;
+  cursor: pointer;
+}
+</style>
